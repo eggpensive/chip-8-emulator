57e0e8eb6a4157140657d15af43a0f4aa1efb0b8
bryan <238042797+eggpensive@users.noreply.github.com>
2026-05-13 21:41:26 +0800

    Define Chip 8 machine structure and update main
    - Create Chip 8 architecture that represents its hardware
    - Add arguments to main function to match SDL main requirement
    - Cast arguments to void as it is not in use yet

--- chip8.c
+++ chip8.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdint.h>
 
-int main()
+struct Chip8
 {
-    //
+    uint8_t registers[16];
+    uint8_t memory[4096];
+    uint16_t index;
+    uint16_t pc;
+    uint16_t stack[16];
+    uint8_t sp;
+    uint8_t delayTimer;
+    uint8_t soundTimer;
+    uint8_t keypad[16];
+    uint32_t video[64 * 32];
+    uint16_t opcode;
+};
+
+int main(int argc, char *argv[])
+{
+    (void)argc; (void)argv;
 
     return 0;
 }
